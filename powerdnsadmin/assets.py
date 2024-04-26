74fe6a0ad958890eaa590a5c15763b8e58df0b4d
Matteo <matteo.dallaba@mynet.it>
2024-04-26 16:56:19 +0200

    Inserimento asset

--- powerdnsadmin/assets.py
+++ powerdnsadmin/assets.py
@@ -10,57 +10,57 @@ class ConcatFilter(Filter):
 
 
 css_login = Bundle(
-    'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    'node_modules/icheck/skins/square/blue.css',
-    'node_modules/admin-lte/dist/css/adminlte.css',
+    'assets/css/all.css',
+    'assets/css/blue.css',
+    'assets/css/adminlte.css',
     filters=('rcssmin', 'cssrewrite'),
     output='generated/login.css')
 
 js_login = Bundle(
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/icheck/icheck.js',
-    'node_modules/knockout/build/output/knockout-latest.js',
+    'assets/js/jquery.js',
+    'assets/js/bootstrap.js',
+    'assets/js/icheck.js',
+    'assets/js/knockout-latest.js',
     'custom/js/custom.js',
     filters=(ConcatFilter, 'rjsmin'),
     output='generated/login.js')
 
 js_validation = Bundle(
-    'node_modules/bootstrap-validator/dist/validator.js',
+    'assets/js/validator.js',
     output='generated/validation.js')
 
 css_main = Bundle(
-    'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css',
-    'node_modules/icheck/skins/square/blue.css',
-    'node_modules/multiselect/css/multi-select.css',
-    'node_modules/admin-lte/dist/css/adminlte.css',
+    'assets/css/all.css',
+    'assets/css/dataTables.bootstrap4.css',
+    'assets/css/blue.css',
+    'assets/css/multi-select.css',
+    'assets/css/adminlte.css',
     'custom/css/custom.css',
-    'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css',
+    'assets/css/bootstrap-datepicker.css',
     filters=('rcssmin', 'cssrewrite'),
     output='generated/main.css')
 
 js_main = Bundle(
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/jquery-ui-dist/jquery-ui.js',
-    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-    'node_modules/datatables.net/js/jquery.dataTables.js',
-    'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js',
-    'node_modules/jquery-sparkline/jquery.sparkline.js',
-    'node_modules/jquery-slimscroll/jquery.slimscroll.js',
-    'node_modules/jquery-validation/dist/jquery.validate.js',
-    'node_modules/icheck/icheck.js',
-    'node_modules/fastclick/lib/fastclick.js',
-    'node_modules/moment/moment.js',
-    'node_modules/admin-lte/dist/js/adminlte.js',
-    'node_modules/multiselect/js/jquery.multi-select.js',
-    'node_modules/datatables.net-plugins/sorting/natural.js',
-    'node_modules/jtimeout/src/jTimeout.js',
-    'node_modules/jquery.quicksearch/src/jquery.quicksearch.js',
-    'node_modules/knockout/build/output/knockout-latest.js',
+    'assets/js/jquery.js',
+    'assets/js/jquery-ui.js',
+    'assets/js/bootstrap.bundle.js',
+    'assets/js/jquery.dataTables.js',
+    'assets/js/dataTables.bootstrap4.js',
+    'assets/js/jquery.sparkline.js',
+    'assets/js/jquery.slimscroll.js',
+    'assets/js/jquery.validate.js',
+    'assets/js/icheck.js',
+    'assets/js/fastclick.js',
+    'assets/js/moment.js',
+    'assets/js/adminlte.js',
+    'assets/js/jquery.multi-select.js',
+    'assets/js/natural.js',
+    'assets/js/jTimeout.js',
+    'assets/js/jquery.quicksearch.js',
+    'assets/js/knockout-latest.js',
     'custom/js/app-authentication-settings-editor.js',
     'custom/js/custom.js',
-    'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
+    'assets/js/bootstrap-datepicker.js',
     filters=(ConcatFilter, 'rjsmin'),
     output='generated/main.js')
 
